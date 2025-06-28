890cc4f38f68e5c38b5de67ccc8f66ca10a19a03
Julia Fokanova <oncologmma@mail.ru>
2025-06-28 19:26:28 +0300

    feat: create adaptive section - Skills
    - create adaptive Skills component
    - add devicons
    - update packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import About from './components/sections/About';
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import Main from './components/layout/Main';
+import Skills from './components/sections/Skills';
 import TopBar from '@/components/layout/TopBar';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Main>
         <About />
+        <Skills />
       </Main>
       <Footer />
     </>
---
+++ src/components/ui/Skill/Skill.tsx
@@ -0,0 +1,17 @@
+import './skill.sass';
+
+interface ISkillProps {
+  src: string;
+  text: string;
+}
+
+const Skill = ({ src, text }: ISkillProps) => {
+  return (
+    <li className="skill">
+      <img className="skill__icon" src={src} alt={text} />
+      <span className="skill__text">{text}</span>
+    </li>
+  );
+};
+
+export default Skill;
